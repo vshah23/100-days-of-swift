e0928168eb254f18f906d96667f80c0820edf8fb
vikas <vik.k.shah@gmail.com>
2023-08-12 23:29:17 -0500

    Add WeSplit project

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Vikas Shah on 8/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vikas Shah on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
